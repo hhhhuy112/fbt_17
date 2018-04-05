b2099e627af27a2503c8524c5203841e23bc84de
hhhhuy112 <minhhuyho2011@gmail.com>
2018-04-05 11:34:53 +0700

    task2

--- database/migrations/2014_10_12_100000_create_password_resets_table.php
+++ database/migrations/2014_10_12_100000_create_password_resets_table.php
@@ -14,9 +14,11 @@ class CreatePasswordResetsTable extends Migration
             $table->timestamp('created_at')->nullable();
         });
     }
-    
+
     public function down()
     {
         Schema::dropIfExists('password_resets');
     }
+    ashdhgasysdghasasgdhassgdhsagdhasgshdgasshd
+    Ssafsa
 }
